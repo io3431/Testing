d15bb38fa69e5c721408dc5424585441d373bb90
Artem <03.artem.yarosh@gmail.com>
2022-12-28 11:03:34 +0300

    add-all

---
+++ DesktopAuth/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DesktopAuth
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RefreshBtn.IsEnabled = false;
+            CodeBx.IsEnabled = false;
+            InputBtn.IsEnabled = false;
+            PasswordBx.IsEnabled = false;
+        }
+
+        string code;
+        Random random = new Random();
+        static DispatcherTimer timer = new DispatcherTimer();
+
+        private void RefreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+            code = random.Next(1000, 9999).ToString();
+            if (MessageBox.Show(code, "КОД", MessageBoxButton.OK, MessageBoxImage.Information) == MessageBoxResult.OK)
+            {
+                timer.Interval = TimeSpan.FromSeconds(10);
+                timer.Tick += new EventHandler(timerTick);
+                timer.Start();
+                CodeBx.IsEnabled = true;
+            }
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            code = null;
+            timer.Stop();
+        }
+
+        private void NumberBx_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                using (var db = new Entities())
+                {
+                    var number = db.Users.FirstOrDefault(u => u.Number == NumberBx.Text);
+                    if (number != null)
+                    {
+                        PasswordBx.IsEnabled = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Номер не верный");
+                    } 
+                }
+
+            }
+        }
+
+        private void PasswordBx_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                using (var db = new Entities())
+                {
+                    var password = db.Users.FirstOrDefault(u => u.Number == NumberBx.Text & u.Password == PasswordBx.Password);
+                    if (password == null)
+                    {
+                        CodeBx.IsEnabled = true;
+                        RefreshBtn.IsEnabled = true;
+                    }
+                    else
+                        MessageBox.Show("Пароль не верный");
+                }
+            }
+        }
+            private void CodeBx_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (code == CodeBx.Text)
+                    InputBtn.IsEnabled = true;
+                else
+                    MessageBox.Show("Код не верный");
+            }
+        }
+
+        private void InputBtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new Entities())
+            {
+                var user = db.Users.FirstOrDefault(u => u.Number == NumberBx.Text & u.Password == PasswordBx.Password);
+                if (user.Acc == true)
+                    MessageBox.Show("Добро пожаловать, Администратор");
+                else
+                    MessageBox.Show("Добро пожаловать, Пользователь");
+            }
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CodeBx.Text = null;
+            PasswordBx.Password = null;
+            NumberBx.Text = null;
+        }
+    }
+}
---
+++ DesktopAuthTests/MainWindowTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesktopAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopAuth.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [TestMethod()]
+        public void CheckingPasswordTest()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+        [TestMethod()]
+        public void CheckingPasswordTest1()
+        {
+            //Arrange
+            string password = "1526";
+            //Act
+            //Assert
+        }
+        [TestMethod()]
+        public void CheckingPasswordTest2()
+        {
+            //Arrange
+            string password = "1518";
+            //Act
+            //Assert
+        }
+        [TestMethod()]
+        public void CheckingPasswordTest3()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void CheckingPasswordTest4()
+        {
+            Assert.Fail();
+        }
+    }
+}
